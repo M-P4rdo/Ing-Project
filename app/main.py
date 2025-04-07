392831da635d43b7a94fcc1382c7120c87616517
Miguel A. Pardo Alvarez <miguelpardoalvare50@gmail.com>
2025-04-06 20:19:58 -0500

    Actualizacion de Informacion

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from app.database import init_db
+from app.routers import car
 
 app = FastAPI()
+app.include_router(car.router)
 
 @app.on_event("startup")
 async def startup():
